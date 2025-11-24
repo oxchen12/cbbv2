cbff353f90c7b296e90e003d870e11fe244ace08
oxchen12 <emailoxc@gmail.com>
2025-11-24 09:28:40 -0500

    Added module-level logger

--- cbb/__init__.py
+++ cbb/__init__.py
@@ -2,3 +2,7 @@
 This module provides a suite of tools for parsing and analyzing
 college basketball data.
 '''
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
