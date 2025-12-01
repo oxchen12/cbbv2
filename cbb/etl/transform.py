fd6e5bf2047fa95351b642ae3953469ea50131aa
oxchen12 <emailoxc@gmail.com>
2025-11-30 23:48:27 -0500

    Implemented schedule transform, added skeletons

--- cbb/etl/transform.py
+++ cbb/etl/transform.py
@@ -25,8 +25,12 @@ logger = logging.getLogger(__name__)
 
 DEFAULT_SEASON_START = dt.date(dt.date.today().year, 7, 1)
 CALENDAR_DT_FORMAT = '%Y-%m-%dT%H:%MZ'
+SCHEDULE_KEEP = (
+    'id', 'teams', 'date',
+    'tbd', 'status', 'venue'
+)
 # TODO: figure out what the hell this is
-SEMAPHORE = asyncio.semaphore(50)
+SEMAPHORE = asyncio.Semaphore(50)
 
 
 def _get_rep_dates(start: str,
@@ -53,7 +57,7 @@ def _get_rep_dates(start: str,
     return rep_dates
 
 
-async def extract_from_schedule(season: int) -> pl.DataFrame:
+async def transform_from_schedule(season: int) -> pl.DataFrame:
     '''
     Extract data from the schedules for the given season.
     Populates Games, Venues, Statuses.
@@ -75,17 +79,110 @@ async def extract_from_schedule(season: int) -> pl.DataFrame:
         ]
         results = await asyncio.run(*tasks)
 
-    results = [
-        res['page']['content']['events']
+    schedule = [
+        {
+            k: v
+            for k, v in event.items()
+            if k in SCHEDULE_KEEP
+        }
         for res in results
+        for date in res['page']['content']['events'].values()
+        for event in date
     ]
+
+    games_inter = (
+        pl.from_dicts(schedule)
+        .lazy()
+        .with_columns(
+            pl.col('id').cast(pl.Int64),
+            pl.col('date').str.to_datetime(time_zone='UTC').alias('datetime'),
+            pl.col('teams')
+            .list.to_struct(fields=['home', 'away'])
+            .struct.unnest()
+        )
+        .unnest('home', 'away', separator='_')
+        .with_columns(
+            homeId=pl.col('home_id').cast(pl.Int64),
+            awayId=pl.col('away_id').cast(pl.Int64),
+        )
+        .select(
+            cs.exclude(
+                'date', 'teams', 'home', 'away',
+                cs.starts_with('home_'),
+                cs.starts_with('away_')
+            )
+        )
+    )
+
+    venues = (
+        games_inter
+        .select(pl.col('venue').struct.unnest())
+        .unique()
+        .with_columns(
+            pl.col('id').cast(pl.Int64)
+        )
+        .sort('id')
+        .unnest('address')
+        .collect()
+    )
+    # TODO: insert into Venues
+
+    game_statuses = (
+        games_inter
+        .select(pl.col('status').struct.unnest())
+        .select(
+            pl.col('id').cast(pl.Int64),
+            'state',
+            # remove details with OT specified
+            pl.col('detail').str.replace(r'(.+)/\d*OT$', r'$1')
+        )
+        .unique()
+        .sort('id')
+        .collect()
+    )
+    # TODO: insert into GameStatuses
+
+    games = (
+        games_inter
+        .with_columns(
+            venueId=pl.col('venue').struct.field('id').cast(pl.Int64),
+            statusId=pl.col('status').struct.field('id').cast(pl.Int64)
+        )
+        .select(pl.exclude('venue', 'status'))
+        .sort('datetime')
+        .collect()
+    )
+    # TODO: insert into Games
+
+    # TODO: not yet implemented
+    return pl.DataFrame()
+
+
+async def transform_from_standings(season: int) -> pl.DataFrame:
+    '''
+    Extract data from the schedules for the given season.
+    Populates Teams, Conferences, ConferenceAlignments.
+    '''
+
+    # TODO: not yet implemented
+    return pl.DataFrame()
+
+
+async def transform_from_game(gid: int) -> pl.DataFrame:
+    '''
+    Extract data from the game.
+    Populates Teams, Plays, PlayTypes, Players.
+    Updates Games.
+    '''
+    client = Client()
+    async with client:
+        game = await client.get_raw_game_json(gid)
 
     # TODO: not yet implemented
     return pl.DataFrame()
 
 
 def get_plays(gid: int | str) -> pl.DataFrame:
-    # TODO: maybe just create a config that defines the default league
     '''
     Get the plays from the raw json and perform cleaning and transformation.
     '''
@@ -150,7 +247,7 @@ def get_plays(gid: int | str) -> pl.DataFrame:
         .select(
             'playId', 'sequenceId', 'text',
             'shortDescription', 'playTypeText', 'playTypeId',
-            'shootingPlay', 'pointsAttempted', 'scoringPlay', 'scoreValue',
+            'shootingPlay', 'pointsAttempted', 'scoringPlay',
             'teamId', 'playerId', 'assistPlayerId',
             'period', 'periodDisplay', 'gameClock',
             'awayScore', 'homeScore',
