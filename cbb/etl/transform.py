2397cc43eff17969006eb10dbfa2763185902a0e
oxchen12 <emailoxc@gmail.com>
2025-12-04 19:58:34 -0500

    Bug, schema fixes and implemented transform_from_game

--- cbb/etl/transform.py
+++ cbb/etl/transform.py
@@ -54,7 +54,11 @@ def _get_rep_dates(start: str,
     return rep_dates
 
 
-async def transform_from_schedule(season: int) -> pl.DataFrame:
+def _get_season(date: dt.date) -> int:
+    return int(date.month < 7) + date.year
+
+
+async def transform_from_schedule(client: AsyncClient, season: int) -> pl.DataFrame:
     '''
     Extract data from the schedules for the given season.
     Populates Games, Venues, Statuses.
@@ -68,15 +72,15 @@ async def transform_from_schedule(season: int) -> pl.DataFrame:
     season = init_schedule['page']['content']['season']
     rep_dates = _get_rep_dates(season['startDate'], season['endDate'])
 
-    client = AsyncClient()
+    # client = AsyncClient()
     async with client:
         tasks = [
             client.get_raw_schedule_json(date)
             for date in rep_dates
         ]
-        schedule_results = await asyncio.run(*tasks)
+        schedule_results = await asyncio.gather(*tasks)
 
-    schedule_json = [
+    events = [
         {
             k: v
             for k, v in event.items()
@@ -87,8 +91,9 @@ async def transform_from_schedule(season: int) -> pl.DataFrame:
         for event in date
     ]
 
+    # TODO: consider moving all of these to external functions
     games_inter = (
-        pl.from_dicts(schedule_json)
+        pl.from_dicts(events)
         .lazy()
         .with_columns(
             pl.col('id').cast(pl.Int64),
@@ -152,24 +157,24 @@ async def transform_from_schedule(season: int) -> pl.DataFrame:
     # TODO: insert into Games
 
     # TODO: not yet implemented
-    return pl.DataFrame()
+    return games
 
 
-async def transform_from_standings(season: int) -> pl.DataFrame:
+async def transform_from_standings(client: AsyncClient, season: int) -> pl.DataFrame:
     '''
     Extract data from the schedules for the given season.
     Populates Teams, Conferences, ConferenceAlignments.
     '''
-    client = AsyncClient()
+    # client = AsyncClient()
     async with client:
         standings_json_raw = await client.get_raw_standings_json(season)
-        standings_json = standings_json_raw['page']['content']
+    standings_content = standings_json_raw['page']['content']
 
     conferences = (
-        pl.from_dicts(standings_json['headerscoreboard']['collegeConfs'])
+        pl.from_dicts(standings_content['headerscoreboard']['collegeConfs'])
         .filter(pl.col('name').ne('NCAA Division I'))
         .select(
-            pl.col('groupId').cast(pl.Int64).alias('cid'),
+            pl.col('groupId').cast(pl.Int64).alias('id'),
             'name',
             pl.col('shortName').alias('abbrev')
         )
@@ -177,7 +182,7 @@ async def transform_from_standings(season: int) -> pl.DataFrame:
     # TODO: insert into Conferences
 
     teams_confs = (
-        pl.from_dicts(standings_json['standings']['groups']['groups'])
+        pl.from_dicts(standings_content['standings']['groups']['groups'])
         .lazy()
         .select(
             pl.col('name').alias('confName'),
@@ -187,14 +192,16 @@ async def transform_from_standings(season: int) -> pl.DataFrame:
         .unnest('standings')
         .unnest('team')
         .join(
-            conferences.select('cid', 'name'),
+            conferences.select(
+                pl.col('cid').alias('conference_id'),
+                'name'
+            ),
             left_on='confName',
             right_on='name'
         )
         .select(
-            'cid',
-            pl.col('id').alias('tid').cast(pl.Int64),
-            pl.col('displayName').alias('name'),
+            'conference_id',
+            pl.col('id').alias('team_id').cast(pl.Int64),
             'location',
             pl.col('shortDisplayName').alias('mascot'),
             'abbrev'
@@ -203,7 +210,7 @@ async def transform_from_standings(season: int) -> pl.DataFrame:
 
     teams = (
         teams_confs
-        .select(pl.exclude('cid'))
+        .select(pl.exclude('conference_id'))
         .collect()
     )
     # TODO: insert into Teams
@@ -211,7 +218,8 @@ async def transform_from_standings(season: int) -> pl.DataFrame:
     conference_alignments = (
         teams_confs
         .select(
-            'cid', 'tid',
+            'conference_id',
+            pl.col('team_id'),
             pl.lit(season).alias('season')
         )
         .collect()
@@ -222,15 +230,230 @@ async def transform_from_standings(season: int) -> pl.DataFrame:
     return pl.DataFrame()
 
 
-async def transform_from_game(gid: int) -> pl.DataFrame:
+def _transform_box(json_raw: dict[str, Any],
+                   team_id: int) -> pl.DataFrame:
+    return (
+        pl.from_dicts(json_raw)
+        .lazy()
+        .unnest('athlete')
+        .with_columns(
+            pl.col('shortName')
+            .str.extract(r'[A-Za-z]+\. (.+)')
+            .alias('last_name'),
+        )
+        .select(
+            pl.col('id').cast(pl.Int64),
+            pl.col('displayName')
+            .str.strip_suffix(pl.col('last_name'))
+            .str.strip_chars(' ')
+            .alias('first_name'),
+            'last_name',
+            pl.col('position')
+            .struct.field('abbreviation')
+            .alias('position'),
+            pl.col('starter').alias('started'),
+            ~pl.col('didNotPlay').alias('played'),
+            'ejected',
+            pl.col('jersey').cast(pl.Int64),
+            pl.lit(team_id).alias('team_id')
+        )
+    )
+
+
+async def transform_from_game(client: AsyncClient, gid: int) -> pl.DataFrame:
     '''
     Extract data from the game.
-    Populates Teams, Plays, PlayTypes, Players.
-    Updates Games.
+    Populates Plays, PlayTypes, Players, PlayerSeasons.
+    Updates Teams, Games.
     '''
-    client = AsyncClient()
+    # client = AsyncClient()
     async with client:
         game_json_raw = await client.get_raw_game_json(gid)
+    attendance = game_json_raw['gameInfo']['attendance']
+    competition_raw = game_json_raw['header']['competitions'][0]
+    competitors_raw = competition_raw.pop('competitors')
+    away_id, home_id = (
+        int(x['team']['id'])
+        for x in game_json_raw['boxscore']['players']
+    )
+    away_box_raw, home_box_raw = (
+        x['statistics'][0]
+        for x in game_json_raw['boxscore']['players']
+    )
+
+    teams = (
+        pl.from_dicts(competitors_raw)
+        .unnest('team', separator='_')
+        .unnest('team_groups', separator='_')
+        .select(
+            pl.col('id').cast(pl.Int64),
+            pl.col('team_color').alias('color'),
+            pl.col('team_alternateColor').alias('alt_color')
+        )
+    )
+    # TODO: update Teams
+
+    games = (
+        pl.from_dicts(competition_raw)
+        .select(
+            pl.col('id').cast(pl.Int64).alias('game_id'),
+            pl.col('date').str.to_date(format='%Y-%m-%dT%H:%MZ'),
+            pl.lit(homeId).cast(pl.Int64).alias('home_id'),
+            pl.lit(awayId).cast(pl.Int64).alias('away_id'),
+            pl.col('neutralSite').alias('is_neutral_site'),
+            pl.col('conferenceCompetition').alias('is_conference'),
+            pl.col('shotChartAvailable').alias('has_shot_chart'),
+            pl.lit(attendance).alias('attendance')
+        )
+    )
+    # TODO: update Games
+
+    plays_inter = (
+        pl.from_dicts(game_json_raw['plays'])
+        .lazy()
+        .unnest(
+            'type',
+            'period',
+            'clock',
+            'coordinate',
+            'team',
+            separator='_'
+        )
+        .with_columns(
+            pl.col('clock_displayValue').str.split(':')
+        )
+        .with_columns(
+            game_id=pl.lit(gid),
+            player_id=(
+                pl.col('participants')
+                .list.first()
+                .struct.field('*')
+                .struct.field('*')
+                .cast(pl.Int64)
+            ),
+            assist_id=(
+                pl.when(pl.col('participants').list.len().gt(1))
+                .then(
+                    pl.col('participants')
+                    .list.last()
+                    .struct.field('*')
+                    .struct.field('*')
+                    .cast(pl.Int64)
+                )
+                .otherwise(None)
+            ),
+            period=pl.col('period_number'),
+            period_display=pl.col('period_displayValue'),
+            sequence_id=pl.col('sequenceNumber').cast(pl.Int64),
+            play_type_id=pl.col('type_id').cast(pl.Int64),
+            play_type_text=pl.col('type_text'),
+            team_id=pl.col('team_id').cast(pl.Int64),
+            x_coord=pl.col('coordinate_x'),
+            y_coord=pl.col('coordinate_y'),
+            clock_minutes=pl.col(
+                'clock_displayValue').list.get(0).cast(pl.Int64),
+            clock_seconds=pl.col(
+                'clock_displayValue').list.get(1).cast(pl.Int64),
+            description=pl.col('shortDescription'),
+            away_score=pl.col('awayScore'),
+            home_score=pl.col('homeScore'),
+            is_shot=pl.col('shootingPlay'),
+            is_score=pl.col('scoringPlay'),
+            points_attempted=pl.col('pointsAttempted'),
+            # TODO: time zone logic from venue
+            timestamp=pl.col('wallclock').cast(
+                pl.Datetime)  # .dt.convert_time_zone(tz),
+        )
+        .select(
+            'game_id', 'sequence_id', 'text',
+            'description', 'play_type_id', 'play_type_text',
+            'is_shot', 'points_attempted', 'is_score',
+            'team_id', 'player_id', 'assist_id',
+            'period', 'period_display',
+            'clock_minutes', 'clock_seconds',
+            'away_score', 'home_score',
+            'x_coord', 'y_coord',
+            'timestamp'
+        )
+    )
+
+    plays = (
+        plays_inter
+        .select(
+            'game_id', 'sequence_id', 'play_type_id',
+            'points_attempted', 'is_score',
+            'team_id', 'player_id', 'assist_id',
+            'period', 'game_clock_minutes', 'game_clock_seconds',
+            'home_score', 'away_score', 'x_coord', 'y_coord',
+            'timestamp'
+        )
+        .collect()
+    )
+    # TODO: insert into Plays
+
+    play_types = (
+        plays_inter
+        .select(
+            pl.col('play_type_id').alias('id'),
+            pl.col('play_type_text').alias('description'),
+            pl.col('is_shot')
+        )
+        .collect()
+    )
+    # TODO: insert into PlayTypes
+
+    players_inter = pl.concat(
+        [
+            _transform_box(away_json_box, away_id),
+            _transform_box(home_json_box, home_id)
+        ],
+        how='vertical'
+    )
+
+    players = (
+        players_inter
+        .select(
+            'id',
+            'first_name',
+            'last_name',
+            'position'
+        )
+        .collect()
+    )
+    # TODO: insert into Players
+
+    season = _get_season(
+        games
+        .item(row=0, col='date')
+    )
+    player_seasons = (
+        players_inter
+        .select(
+            pl.col('id').alias('player_id'),
+            'team_id',
+            pl.lit(season).alias('season'),
+            'jersey'
+        )
+        .collect()
+    )
+    # TODO: insert into PlayerSeasons
+
+    game_id = (
+        games
+        .item(row=0, col='id')
+    )
+    game_logs = (
+        players_inter
+        .select(
+            pl.col('id').alias('player_id'),
+            pl.lit(game_id),
+            'played',
+            'started',
+            'ejected'
+        )
+        .collect()
+    )
+    # TODO: insert into GameLogs
 
     # TODO: not yet implemented
     return pl.DataFrame()
@@ -256,14 +479,15 @@ def get_plays(gid: int | str) -> pl.DataFrame:
             pl.col('clock_displayValue').str.split(':')
         )
         .with_columns(
-            playerId=(
+            game_id=pl.lit(gid),
+            player_id=(
                 pl.col('participants')
                 .list.first()
                 .struct.field('*')
                 .struct.field('*')
                 .cast(pl.Int64)
             ),
-            assistPlayerId=(
+            assist_id=(
                 pl.when(pl.col('participants').list.len().gt(1))
                 .then(
                     pl.col('participants')
@@ -275,37 +499,36 @@ def get_plays(gid: int | str) -> pl.DataFrame:
                 .otherwise(None)
             ),
             period=pl.col('period_number'),
-            periodDisplay=pl.col('period_displayValue'),
-            playId=pl.col('id').cast(pl.Int64),
-            sequenceId=pl.col('sequenceNumber').cast(pl.Int64),
-            playTypeId=pl.col('type_id').cast(pl.Int64),
-            playTypeText=pl.col('type_text'),
-            teamId=pl.col('team_id').cast(pl.Int64),
-            coordinateX=pl.col('coordinate_x'),
-            coordinateY=pl.col('coordinate_y'),
-            gameClock=pl.duration(
-                minutes=pl.col('clock_displayValue').list.get(
-                    0).cast(pl.Int64),
-                seconds=pl.col('clock_displayValue').list.get(
-                    1).cast(pl.Int64),
-                time_unit='ms'
-            ),
+            period_display=pl.col('period_displayValue'),
+            sequence_id=pl.col('sequenceNumber').cast(pl.Int64),
+            play_type_id=pl.col('type_id').cast(pl.Int64),
+            play_type_text=pl.col('type_text'),
+            team_id=pl.col('team_id').cast(pl.Int64),
+            x_coord=pl.col('coordinate_x'),
+            y_coord=pl.col('coordinate_y'),
+            clock_minutes=pl.col(
+                'clock_displayValue').list.get(0).cast(pl.Int64),
+            clock_seconds=pl.col(
+                'clock_displayValue').list.get(1).cast(pl.Int64),
+            description=pl.col('shortDescription'),
+            away_score=pl.col('awayScore'),
+            home_score=pl.col('homeScore'),
+            is_shot=pl.col('shootingPlay'),
+            is_score=pl.col('scoringPlay'),
+            points_attempted=pl.col('pointsAttempted'),
             # TODO: time zone logic from venue
             timestamp=pl.col('wallclock').cast(
                 pl.Datetime)  # .dt.convert_time_zone(tz),
         )
-        .drop(
-            'id', 'participants', 'sequenceNumber', 'wallclock',
-            cs.contains('_')
-        )
         .select(
-            'playId', 'sequenceId', 'text',
-            'shortDescription', 'playTypeText', 'playTypeId',
-            'shootingPlay', 'pointsAttempted', 'scoringPlay',
-            'teamId', 'playerId', 'assistPlayerId',
-            'period', 'periodDisplay', 'gameClock',
-            'awayScore', 'homeScore',
-            'coordinateX', 'coordinateY',
+            'game_id', 'sequence_id', 'text',
+            'description', 'play_type_id', 'play_type_text',
+            'is_shot', 'points_attempted', 'is_score',
+            'team_id', 'player_id', 'assist_id',
+            'period', 'period_display',
+            'clock_minutes', 'clock_seconds',
+            'away_score', 'home_score',
+            'x_coord', 'y_coord',
             'timestamp'
         )
     )
