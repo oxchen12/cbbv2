f1db26fd99e5e24eb42d0e53821b1561f57a45f1
oxchen12 <emailoxc@gmail.com>
2025-12-10 14:38:50 -0500

    Updated schema to conform to DuckDB

--- cbb/etl/sql/create_tables.sql
+++ cbb/etl/sql/create_tables.sql
@@ -1,3 +1,68 @@
+CREATE TABLE IF NOT EXISTS Venues (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    city VARCHAR,
+    state VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS Teams (
+    id INTEGER PRIMARY KEY,
+    location VARCHAR,
+    mascot VARCHAR,
+    abbrev VARCHAR,
+    color VARCHAR(6),
+    alt_color VARCHAR(6)
+);
+
+CREATE TABLE IF NOT EXISTS Conferences (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    abbrev VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ConferenceAlignments (
+    team_id INTEGER,
+    conference_id INTEGER,
+    season INTEGER,
+    PRIMARY KEY (team_id, conference_id, season),
+    FOREIGN KEY (team_id) REFERENCES Teams (id),
+    FOREIGN KEY (conference_id) REFERENCES Conferences (id)
+);
+
+CREATE TABLE IF NOT EXISTS PlayTypes (
+    id INTEGER PRIMARY KEY,
+    description VARCHAR,
+    is_shot BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS Players (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    position VARCHAR(1),
+    height_ft INTEGER,
+    height_in INTEGER,
+    weight INTEGER,
+    birth_city VARCHAR,
+    birth_state VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS PlayerSeasons (
+    player_id INTEGER,
+    team_id INTEGER NOT NULL,
+    season INTEGER NOT NULL,
+    jersey INTEGER,
+    PRIMARY KEY (player_id, team_id, season),
+    FOREIGN KEY (player_id) REFERENCES Players (id),
+    FOREIGN KEY (team_id) REFERENCES Teams (id)
+);
+
+CREATE TABLE IF NOT EXISTS GameStatuses (
+    id INTEGER PRIMARY KEY,
+    state VARCHAR,
+    detail VARCHAR
+);
+
 CREATE TABLE IF NOT EXISTS Games (
     id INTEGER PRIMARY KEY,
     datetime DATETIME,
@@ -17,49 +82,11 @@ CREATE TABLE IF NOT EXISTS Games (
     FOREIGN KEY (status_id) REFERENCES GameStatuses (id)
 );
 
-CREATE TABLE IF NOT EXISTS GameStatuses (
-    id INTEGER PRIMARY KEY,
-    state VARCHAR,
-    detail VARCHAR
-);
-
-CREATE TABLE IF NOT EXISTS Venues (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    city VARCHAR,
-    state VARCHAR
-);
-
-CREATE TABLE IF NOT EXISTS Teams (
-    id INTEGER PRIMARY KEY,
-    location VARCHAR NOT NULL,
-    mascot VARCHAR NOT NULL,
-    -- not sure
-    abbrev VARCHAR NOT NULL,
-    color VARCHAR(6),
-    alt_color VARCHAR(6)
-);
-
-CREATE TABLE IF NOT EXISTS ConferenceAlignments (
-    team_id INTEGER,
-    conference_id INTEGER,
-    season INTEGER,
-    PRIMARY KEY (team_id, conference_id, season),
-    FOREIGN KEY (team_id) REFERENCES Teams (id),
-    FOREIGN KEY (conference_id) REFERENCES Conferences (id)
-);
-
-CREATE TABLE IF NOT EXISTS Conferences (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    abbrev VARCHAR NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS Plays (
     game_id INTEGER,
     sequence_id INTEGER,
     play_type_id INTEGER NOT NULL,
-    points_attempted INTEGER NOT NULL CHECK (points_attempted <= 3),
+    points_attempted INTEGER NOT NULL,
     is_score BOOLEAN NOT NULL,
     team_id INTEGER,
     player_id INTEGER,
@@ -80,41 +107,12 @@ CREATE TABLE IF NOT EXISTS Plays (
     FOREIGN KEY (assist_id) REFERENCES Players (id)
 );
 
-CREATE TABLE IF NOT EXISTS PlayTypes (
-    id INTEGER PRIMARY KEY,
-    description VARCHAR,
-    is_shot BOOLEAN
-);
-
-CREATE TABLE IF NOT EXISTS Players (
-    id INTEGER PRIMARY KEY,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    position VARCHAR(1),
-    height_ft INTEGER,
-    height_in INTEGER,
-    weight INTEGER,
-    birth_city VARCHAR,
-    birth_state VARCHAR
-);
-
-CREATE TABLE IF NOT EXISTS PlayerSeasons (
-    player_id INTEGER,
-    team_id INTEGER NOT NULL,
-    season INTEGER NOT NULL,
-    jersey INTEGER,
-    PRIMARY KEY (player_id, team_id, season),
-    FOREIGN KEY (player_id) REFERENCES Players (id),
-    FOREIGN KEY (team_id) REFERENCES Teams (id)
-);
-
 CREATE TABLE IF NOT EXISTS GameLogs (
     player_id INTEGER,
     game_id INTEGER,
     played BOOLEAN NOT NULL,
     started BOOLEAN NOT NULL,
     ejected BOOLEAN NOT NULL,
-    -- TODO: should there be stats here
     PRIMARY KEY (player_id, game_id),
     FOREIGN KEY (player_id) REFERENCES Players (id),
     FOREIGN KEY (game_id) REFERENCES Games (id)
