13f875f71691329c84fa589ae0debb80adaceedd
oxchen12 <emailoxc@gmail.com>
2025-12-10 00:22:46 -0500

    Added player updating

--- cbb/etl/transform.py
+++ cbb/etl/transform.py
@@ -13,12 +13,12 @@ import sqlite3
 
 from .scrape import (
     get_raw_schedule_json,
-    get_raw_game_json,
     AsyncClient
 )
 from .database import (
     Table,
     WriteAction,
+    insert_to_db,
     inserts_to_db
 )
 from .date import (
@@ -74,6 +74,9 @@ async def transform_from_schedule(
     Extract data from the schedules for the given season.
     Populates Games, Venues, Statuses.
     '''
+    # TODO: consider making this function only grab one day's schedule
+    #       this way, the `load` module will be able to control what days
+    #       are captured based on existing data in the database
     validate_season(season)
 
     # use one sync request to get calendar
@@ -102,11 +105,11 @@ async def transform_from_schedule(
         for event in date
     ]
 
-    # TODO: consider moving all of these to external functions
     if len(events) == 0:
         logger.debug('No events found for %d', season)
         return 0
 
+    # TODO: consider moving all of these to external functions
     games_inter = (
         pl.from_dicts(events)
         .lazy()
@@ -189,7 +192,7 @@ async def transform_from_standings(
     season: int
 ) -> int:
     '''
-    Extract data from the schedules for the given season.
+    Extract data from the standings page for the given season.
     Populates Teams, Conferences, ConferenceAlignments.
     '''
     validate_season(season)
@@ -303,10 +306,10 @@ def _transform_box(json_raw: dict[str, Any],
 async def transform_from_game(
     conn: sqlite3.Connection,
     client: AsyncClient,
-    gid: int
+    game_id: int
 ) -> pl.DataFrame:
     '''
-    Extract data from the game.
+    Extract data from the game page.
     Populates Plays, PlayTypes, Players, PlayerSeasons.
     Updates Teams, Games.
     '''
@@ -364,7 +367,7 @@ async def transform_from_game(
             pl.col('clock_displayValue').str.split(':')
         )
         .with_columns(
-            game_id=pl.lit(gid),
+            game_id=pl.lit(game_id),
             player_id=(
                 pl.col('participants')
                 .list.first()
@@ -428,7 +431,6 @@ async def transform_from_game(
         )
         .collect()
     )
-    # TODO: insert into Plays
 
     play_types = (
         plays_inter
@@ -509,3 +511,55 @@ async def transform_from_game(
 
     # TODO: not yet implemented
     return sum(rows)
+
+
+async def transform_from_player(
+    conn: sqlite3.Connection,
+    client: AsyncClient,
+    player_id: int
+) -> int:
+    '''
+    Extract data from the player page.
+    Updates Players.
+    '''
+    player_raw = await client.get_raw_player_json(player_id)
+    athlete = player_raw['page']['content']['player']['plyrHdr']['ath']
+
+    players = (
+        pl.from_dict(athlete)
+        .select(
+            pl.lit(player_id).alias('id'),
+            cs.matches(r'^htwt$')
+            .str.split(', ')
+            .list.to_struct(fields=['ht', 'wt'])
+            .struct.unnest(),
+            cs.matches(r'^dob$')
+            .str.replace(r' (\d+)', ''),
+            cs.matches(r'^brthpl$')
+            .str.split(', ')
+            .list.to_struct(fields=['birth_city', 'birth_state'])
+            .struct.unnest()
+        )
+        .select(
+            pl.exclude('htwt', 'brthpl', 'ht', 'wt'),
+            cs.matches(r'^ht$')
+            .str.replace_all(r'[\'"]', '')
+            .str.split(' ')
+            .list.to_struct(fields=['height_ft', 'height_in'])
+            .struct.unnest()
+            .cast(pl.Int64),
+            cs.matches(r'^wt$')
+            .str.replace(' lbs', '')
+            .cast(pl.Int64)
+            .alias('weight')
+        )
+    )
+
+    rows = insert_to_db(
+        players,
+        Table.PLAYERS,
+        conn,
+        WriteAction.UPDATE
+    )
+
+    return rows
